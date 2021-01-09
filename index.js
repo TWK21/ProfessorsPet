82dd0083ebf70761afc337d1a73a0ab8619ae3ed
Runningsloths <slothstrash@gmail.com>
2021-01-09 18:10:59 -0500

    implemented list ended meeting instances

--- index.js
+++ index.js
@@ -42,3 +42,27 @@ app.get('/redirect', async (req, res) => {
 });
 
 // Zoom API
+
+function listEndedMeetingInstances(meetingId) {
+	if (req.cookies.zoomToken) {
+		try {
+			const url = `https://api.zoom.us/v2/past_meetings/${meetingId}/participants`;
+			const config = {
+				headers: {
+					Authorization: `Bearer ${req.cookies.zoomToken}`
+				}
+			}
+			const { data } = await axios.get(url, config);
+			return data;
+		}
+		catch (err) {
+			throw new Error('listEndedMeetingInstances error');
+		}
+	}
+}
+
+// Module Exports
+
+module.exports = {
+	listEndedMeetingInstances
+};
