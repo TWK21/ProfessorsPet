84785d8873fbaa503ee407c3e6a20c071b5eeea4
Runningsloths <slothstrash@gmail.com>
2021-01-09 17:52:59 -0500

    zoom oauth

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
+const qs = require('qs');
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const PORT = 5420;
+
+// Express
 
 const app = express();
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
 app.use('/', express.static('dist'));
 app.use(express.json());
+
+// Zoom OAuth
+
+app.get('/redirect', async (req, res) => {
+	if (req.query.code) {
+		const url = 'https://zoom.us/oauth/token';
+		const params = {
+			grant_type: 'authorization_code',
+			code: req.query.code,
+			redirect_uri: 'https://zoom.piyo.cafe/redirect'
+		};
+		const config = {
+			headers: {
+				Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
+			}
+		};
+		try {
+			const { data } = await axios.post(url, qs.stringify(params), config);
+			res.cookie('zoomToken', data.access_token);
+			res.redirect('/app');
+		}
+		catch (err) {
+			console.error(err);
+		}
+	} 
+});
+
+// Zoom API
