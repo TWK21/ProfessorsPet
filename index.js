e57aeeeecaa4424c1183f915997011e4c912c815
lanpai <lan@piyo.cafe>
2021-01-10 01:19:05 -0500

    Updated routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const axios = require('axios');
-const cookieParser = require('cookie-parser');
-const qs = require('qs');
+const express = require("express");
+const axios = require("axios");
+const cookieParser = require("cookie-parser");
+const qs = require("qs");
+
+const attendanceRoutes = require("./attendanceRoutes.js");
+const recordingRoutes = require("./recordingRoutes.js");
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -12,32 +15,35 @@ const PORT = 5420;
 const app = express();
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 
-app.use('/', express.static('dist'));
+app.use("/", express.static("dist"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(attendanceRoutes);
+app.use(recordingRoutes);
 
 // Zoom OAuth
 
-app.get('/redirect', async (req, res) => {
-	if (req.query.code) {
-		const url = 'https://zoom.us/oauth/token';
-		const params = {
-			grant_type: 'authorization_code',
-			code: req.query.code,
-			redirect_uri: 'https://zoom.piyo.cafe/redirect'
-		};
-		const config = {
-			headers: {
-				Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`
-			}
-		};
-		try {
-			const { data } = await axios.post(url, qs.stringify(params), config);
-			res.cookie('zoomToken', data.access_token);
-			res.redirect('/app');
-		}
-		catch (err) {
-			console.error(err);
-		}
-	} 
+app.get("/redirect", async (req, res) => {
+  if (req.query.code) {
+    const url = "https://zoom.us/oauth/token";
+    const params = {
+      grant_type: "authorization_code",
+      code: req.query.code,
+      redirect_uri: "https://zoom.piyo.cafe/redirect",
+    };
+    const config = {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${CLIENT_ID}:${CLIENT_SECRET}`
+        ).toString("base64")}`,
+      },
+    };
+    try {
+      const { data } = await axios.post(url, qs.stringify(params), config);
+      res.cookie("zoomToken", data.access_token);
+      res.redirect("/app");
+    } catch (err) {
+      console.error(err);
+    }
+  }
 });
