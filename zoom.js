d7cc8da7170a811dcf7e2558ee1f7a0171ff50c4
Runningsloths <slothstrash@gmail.com>
2021-01-09 19:40:55 -0500

    implemented list ended meeting instances

--- zoom.js
+++ zoom.js
@@ -1,25 +1,54 @@
 const axios = require('axios');
+const _ = require('lodash');
 
-async function listEndedMeetingInstances(meetingId) {
-	if (req.cookies.zoomToken) {
-		try {
-			const url = `https://api.zoom.us/v2/past_meetings/${meetingId}/participants`;
-			const config = {
-				headers: {
-					Authorization: `Bearer ${req.cookies.zoomToken}`
-				}
-			}
-			const { data } = await axios.get(url, config);
-			return data;
-		}
-		catch (err) {
-			throw new Error('listEndedMeetingInstances error');
-		}
-	}
+/*
+listEndedMeetingInstances(token, meetingId)
+  returns 
+    [{
+      uuid
+      start_time
+    }...]
+*/
+
+async function listEndedMeetingInstances(token, meetingId) {
+  try {
+    const url = `https://api.zoom.us/v2/past_meetings/${meetingId}/instances`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    const { data } = await axios.get(url, config);
+
+    return data.meetings;
+  }
+  catch (err) {
+    throw err;
+  }
+}
+
+async function getPastMeetingParticipants(token, meetingUUID) {
+  try {
+    const encodedUUID = encodeURIComponent(encodeURIComponent(meetingUUID));
+    const url = `https://api.zoom.us/v2/past_meetings/${encodedUUID}/participants`;
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    const { data } = await axios.get(url, config);
+    const alphebetizedList = _.orderBy(data.participants, [part => part.name], ['desc']);
+
+    return alphebetizedList;
+  }
+  catch (err) {
+    throw err;
+  }
 }
 
 // Module Exports
 
 module.exports = {
-	listEndedMeetingInstances
+  listEndedMeetingInstances,
+  getPastMeetingParticipants
 };
